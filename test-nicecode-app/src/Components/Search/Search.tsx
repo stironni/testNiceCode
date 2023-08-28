c7ba4c9a0bc4e6a4da48dc16943fd8284dd2c7aa
Stir <stironni@student.21-school.ru>
2023-08-28 11:13:30 +0300

    feat: search done

--- test-nicecode-app/src/Components/Search/Search.tsx
+++ test-nicecode-app/src/Components/Search/Search.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./style.module.scss";
 
 function Search () {
 
+    const [isSearch, setIsSearch] = useState(false);
+
+    const isSearchFunc = (t:boolean) =>{
+        if(isSearch) setIsSearch(false)
+        else setIsSearch(true)
+    }
+
     return (
-        <div className={style.main}>
-            <div className={style.leftIcon}>
-                <button className={style.iconSearch}></button>
+        isSearch ? 
+        (
+        <div className={style.mainSearch}>
+            <div className={style.divForInput}>
+                <div className={style.iconForInput}></div>
+                <input className={style.inputWithIcon} type="search" autoFocus/>
             </div>
             <div className={style.rightIcon}>
-                <button className={style.iconFilter}></button>
-                <button className={style.iconPlus}></button>
+                <button className={[style.iconClose, style.button].join(' ')} onClick={() => isSearchFunc(isSearch)}></button>
             </div>
-
         </div>
+        )
+        :
+        (
+            <div className={style.mainSearch}>
+                <div className={style.leftIcon}>
+                    <button className={[style.iconSearch, style.button].join(' ')} onClick={() => isSearchFunc(isSearch)}></button>
+                </div>
+                <div className={style.rightIcon}>
+                    <button className={[style.iconFilter, style.button].join(' ')}></button>
+                    <button className={[style.iconPlus, style.button].join(' ')}></button>
+                </div>
+            </div>
+            )
     )
 }
 
