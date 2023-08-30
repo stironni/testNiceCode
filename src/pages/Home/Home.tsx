4c4305dfa08974726e746df73fb257e7b1562bbe
Stir <stironni@student.21-school.ru>
2023-08-30 20:59:08 +0300

    fixed bugs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import style from './style.module.scss';
-import InfoAboutContact from './../../Components/InfoAboutContact/InfoAboutContact'
-import Tabs from './../../Components/Tabs/Tabs';
-import Search from './../../Components/Search/Search';
+import InfoAboutContact from '../../Components/InfoAboutContact/InfoAboutContact'
+import Tabs from '../../Components/Tabs/Tabs';
+import Search from '../../Components/Search/Search';
 
 function Home () {
 
